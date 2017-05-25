8f0448e3bbf4ccd0f9e7a3513907fd78914d61a8
MasterEwink <ewokisamail@gmail.com>
2017-05-25 07:37:08 +0200

    Ajout d'un header
    Pour pouvoir utiliser les dimensions du réseau dans le système de sauvegarde, j'ai déplacé les directives de préprocesseur du main dans un header, et j'y ai rajouté de quoi gérer les erreurs de sauvegarde.

---
+++ common.h
@@ -0,0 +1,16 @@
+#ifndef DEF_COMMON
+#define DEF_COMMON
+
+#define NB_LIGNES 4     //nombre de lignes de vignettes
+#define NB_COLONNES 5   //nombre de colonnes de vignettes
+#define NB_TESTS NB_LIGNES*NB_COLONNES  //nombre de vignettes
+#define NB_ENTREES 6
+#define NB_BITS 8
+#define COTE_VIGNETTE 4
+#define NB_SORTIES NB_BITS*COTE_VIGNETTE*COTE_VIGNETTE  //nombre de bits pour coder une vignette
+
+
+#define F_NO_ERROR 0
+#define F_ERR_NOT_OPEN 1
+
+#endif // DEF_COMMON
